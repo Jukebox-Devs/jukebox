b7d1ce678e19fac964b66fe910ede9bd2c7df189
snekolen <54336823+snekolen@users.noreply.github.com>
2021-06-20 00:00:27 -0500

    Reorganized countries list into an array dict

--- countries.py
+++ countries.py
@@ -1,22 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 18 15:33:46 2021
+#Checks if country exists within a certain decade and returns list of all countries
 
-@author: helen
-"""
-
-
-def countriesList(year):
-    if year == 1900:
-        countries = ["Argentina", "Austria", "Belgium", "Bhutan", "Bolivia", "Brazil", "Bulgaria", "Canada", 
+countriesArr = [{"year" : 1900, "countries" : ["Argentina", "Austria", "Belgium", "Bhutan", "Bolivia", "Brazil", "Bulgaria", "Canada", 
                      "Chile", "Colombia", "Costa Rica", "Cuba", "Denmark", "Dominican Republic", "Ecuador", 
                      "El Salvador", "Ethiopia", "France", "Germany", "Greece", "Guatemala", "Haiti", "Honduras",
                      "Hungary", "India", "Italy", "Japan", "Liberia", "Mexico", "Montenegro", "Morocco", "Nepal", 
                      "Netherlands", "New Zealand", "Norway", "Panama", "Paraguay", "Peru", "Philippines", "Portugal",
                      "Romania", "Russia", "Serbia", "Spain", "Sweden", "Switzerland", "Turkey", "United Kingdom", 
-                     "United States", "Uruguay", "Venezuela"]
-    elif year == 1910:
-        countries = ["Albania", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", 
+                     "United States", "Uruguay", "Venezuela"]}, 
+                {"year": 1910, "countries" : ["Albania", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", 
                      "Belgium", "Bhutan", "Bolivia", "Canada", "Chile", "China", "Colombia", 
                      "Costa Rica", "Cuba", "Czechoslovakia", "Denmark", "Dominican Republic", "Ecuador",
                      "El Salvador", "Estonia", "Ethiopia", "Finland", "France", "Georgia", "Germany", 
@@ -25,9 +16,8 @@ def countriesList(year):
                      "Morocco", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Norway", "Panama",
                      "Paraguay", "Peru", "Poland", "Portugal", "Romania", "Russia", "Serbia", "South Africa",
                      "Spain", "Sweden", "Switzerland", "Turkey", "United Kingdom", "United States",
-                     "Uruguay", "Venezuela"]
-    elif year == 1920:
-        countries = ["Albania", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Belgium", 
+                     "Uruguay", "Venezuela"]}, 
+                {"year": 1920, "countries" : ["Albania", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Belgium", 
                      "Bolivia", "Brazil", "Bulgaria", "Canada", "Chile", "Colombia", "Costa Rica", "Cuba", 
                      "Czechoslovakia", "Denmark", "Dominican Republic", "Ecuador", "Egypt", "El Salvador",
                      "Estonia", "Ethiopia", "Finland", "France", "Georgia", "Germany", "Greece", "Guatemala", 
@@ -35,9 +25,8 @@ def countriesList(year):
                      "Liberia", "Lithuania", "Mexico", "Mongolia", "Nepal", "Netherlands", "New Zealand", 
                      "Nicaragua", "Norway", "Panama", "Paraguay", "Peru", "Poland", "Portugal", "Romania", 
                      "Soviet Union", "Spain", "Sweden", "Switzerland", "Turkey", "United Kingdom", 
-                     "United States", "Uruguay", "Venezuela", "Yugoslavia"]
-    elif year == 1930:
-        countries = ["Afghanistan", "Albania", "Argentina", "Australia", "Austria", "Belgium", "Bolivia", 
+                     "United States", "Uruguay", "Venezuela", "Yugoslavia"]}, 
+                {"year": 1930, "countries" : ["Afghanistan", "Albania", "Argentina", "Australia", "Austria", "Belgium", "Bolivia", 
                      "Brazil", "Bulgaria", "Canada", "Chile", "Colombia", "Costa Rica", "Cuba", "Czechoslovakia", 
                      "Denmark", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Estonia", "Ethiopia", 
                      "Finland", "France", "Germany", "Greece", "Guatemala", "Haiti", "Honduras", "Hungary", 
@@ -45,9 +34,8 @@ def countriesList(year):
                      "Lithuania", "Mexico", "Mongolia", "Nepal", "Netherlands", "New Zealand", "Nicaragua", 
                      "Norway", "Panama", "Paraguay", "Peru", "Poland", "Portugal", "Romania", "Saudi Arabia", 
                      "Soviet Union", "Spain", "Sweden", "Switzerland", "Thailand", "Turkey", "United Kingdom", 
-                     "United States", "Uruguay", "Yugoslavia"]
-    elif year == 1940:
-        countries = ["Afghanistan", "Albania", "Argentina", "Australia", "Belgium", "Bhutan", "Bolivia", 
+                     "United States", "Uruguay", "Yugoslavia"]}, 
+                {"year" : 1940, "countries" : ["Afghanistan", "Albania", "Argentina", "Australia", "Belgium", "Bhutan", "Bolivia", 
                      "Brazil", "Bulgaria", "Canada", "China", "Colombia", "Costa Rica", "Cuba", "Czechoslovakia", 
                      "Denmark", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Estonia", "Ethiopia", 
                      "Finland", "France", "Germany", "Greece", "Guatemala", "Haiti", "Honduras", "Hungary", 
@@ -56,9 +44,8 @@ def countriesList(year):
                      "Mongolia", "Myanmar", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Norway",
                      "Pakistan", "Panama", "Paraguay", "Peru", "Poland", "Portugal", "Romania", "Saudi Arabia", 
                      "Soviet Union", "Spain", "Sweden", "Switzerland", "Syria", "Taiwan", "Thailand", "Turkey", 
-                     "United Kingdom", "United States", "Uruguay", "Venezuela", "Yugoslavia"]
-    elif year == 1950:
-        countries = ["Afghanistan", "Albania", "Argentina", "Australia", "Austria", "Belgium", "Bhutan", 
+                     "United Kingdom", "United States", "Uruguay", "Venezuela", "Yugoslavia"]}, 
+                {"year" : 1950, "countries" : ["Afghanistan", "Albania", "Argentina", "Australia", "Austria", "Belgium", "Bhutan", 
                      "Bolivia", "Brazil", "Bulgaria", "Cambodia", "Canada", "Chile", "China", "Colombia", 
                      "Costa Rica", "Cuba", "Czechoslovakia", "Denmark", "Dominican Republic", "Ecuador", "Egypt", 
                      "El Salvador", "Ethiopia", "Finland", "France", "East Germany", "West Germany", "Ghana", 
@@ -68,9 +55,8 @@ def countriesList(year):
                      "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Norway", "Pakistan", "Panama", 
                      "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Romania", "Saudi Arabia", 
                      "Soviet Union", "Spain", "Sudan", "Sweden", "Syria", "Taiwan", "Thailand", "Tunisia", 
-                     "Turkey", "United Kingdom", "United States", "Uruguay", "Venezuela", "Vietnam", "Yugoslavia"]
-    elif year == 1960:
-        countries = ["Afghanistan", "Albania", "Algeria", "Argentina", "Australia", "Austria", "Belgium", 
+                     "Turkey", "United Kingdom", "United States", "Uruguay", "Venezuela", "Vietnam", "Yugoslavia"]}, 
+                {"year" : 1960, "countries" : ["Afghanistan", "Albania", "Algeria", "Argentina", "Australia", "Austria", "Belgium", 
                      "Benin", "Bhutan", "Bolivia", "Brazil", "Bulgaria", "Burundi", "Cambodia", "Cameroon", 
                      "Canada", "Chad", "Chile", "China", "Colombia", "Congo", "Costa Rica", "Cuba", "Czechoslovakia", 
                      "Denmark", "Dominican Republic", "Ecuador", "El Savador", "Equatorial Guinea", "Ethiopia", 
@@ -84,9 +70,8 @@ def countriesList(year):
                      "Singapore", "Somalia", "South Africa", "Soviet Union", "Spain", "Sudan", "Sweden", 
                      "Swaziland", "Switzerland", "Syria", "Taiwan", "Thailand", "Trinidad and Tobago", "Tunisia", 
                      "Turkey", "Uganda", "United Kingdom", "United States", "Uruguay", "Venezuela", "Vietnam", 
-                     "Yugoslavia"]
-    elif year == 1970:
-        countries = ["Afghanistan", "Albania", "Algeria", "Angolia", "Argentina", "Australia", "Austria", 
+                     "Yugoslavia"]}, 
+                {"year" : 1970, "countries" : ["Afghanistan", "Albania", "Algeria", "Angolia", "Argentina", "Australia", "Austria", 
                      "Bahamas", "Bangladesh", "Barbados", "Belgium", "Benin", "Bhutan", "Brazil", "Bolivia", 
                      "Cambodia", "Cameroon", "Canada", "Chad", "Chile", "China", "Colombia", "Congo", "Costa Rica", 
                      "Cuba", "Czechoslovakia", "Denmark", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", 
@@ -101,9 +86,8 @@ def countriesList(year):
                      "South Africa", "Soviet Union", "Spain", "Sri Lanka", "Sudan", "Suriname" , "Sweden", 
                      "Swaziland", "Switzerland", "Syria", "Taiwan", "Tanzania", "Trinidad and Tobago", "Tunisia", 
                      "Turkey", "UAE", "United Kingdom", "United States", "Uruguay", "Venezuela", "Vietnam", 
-                     "Yugoslavia"]
-    elif year == 1980: 
-        countries = ["Afghanistan", "Albania", "Algeria", "Angolia", "Argentina", "Australia", "Austria", 
+                     "Yugoslavia"]}, 
+                {"year" : 1980, "countries" : ["Afghanistan", "Albania", "Algeria", "Angolia", "Argentina", "Australia", "Austria", 
                      "Bahamas", "Bangladesh", "Barbados", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", 
                      "Brazil", "Bulgaria", "Cameroon", "Canada", "Chad", "Chile", "China", "Colombia", "Congo", 
                      "Costa Rica", "Cuba", "Czechoslovakia", "Denmark", "Dominican Republic", "Ecuador", 
@@ -119,12 +103,11 @@ def countriesList(year):
                      "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", 
                      "Taiwan", "Tanzania", "Thailand", "Trinidad and Tabago", "Tunisia", "Turkey", "Uganda", 
                      "UAE", "United Kingdom", "United States", "Uruguay", "Venezuela", "Vietnam", "Yugoslavia", 
-                     "Zimbabwe"]
-    elif year == 1990:
-        countries = ["Afghanistan", "Albania", "Algeria", "Angola", "Argentina", "Armenia", "Australia", 
-                     "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belgium", 
-                     "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia", "Brazil", "Bolivia", "Bulgaria", 
-                     "Belarus", "Cambodia", "Cameroon", "Chad", "Chile", "China", "Colombia", "Congo", 
+                     "Zimbabwe"]}, 
+                {"year" : 1990, "countries" : ["Afghanistan", "Albania", "Algeria", "Angola", "Argentina", "Armenia", "Australia", 
+                     "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", 
+                     "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia", "Brazil", "Bulgaria", 
+                     "Cambodia", "Cameroon", "Canada","Chad", "Chile", "China", "Colombia", "Congo", 
                      "Costa Rica", "Croatia", "Cuba", "Czech Republic", "Denmark", "Dominican Republic", 
                      "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Finland", 
                      "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Guatemala", 
@@ -139,9 +122,8 @@ def countriesList(year):
                      "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", 
                      "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Trinidad and Tobago", "Tunisia", "Turkey", 
                      "Turkmenistan", "Uganda", "Ukraine", "UAE", "United Kingdom", "United States", "Uruguay", 
-                     "Uzbekistan", "Venezuela", "Vietnam", "Yugoslavia", "Zimbabwe"]
-    elif year == 2000 or year == 2010 or year == 2020:
-        countries = ["Afghanistan", "Albania", "Algeria", "Angola", "Argentina", "Armenia", "Australia", 
+                     "Uzbekistan", "Venezuela", "Vietnam", "Yugoslavia", "Zimbabwe"]}, 
+                {"year" : 2000, "countries" : ["Afghanistan", "Albania", "Algeria", "Angola", "Argentina", "Armenia", "Australia", 
                      "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", 
                      "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia", "Brazil", "Bulgaria", 
                      "Cambodia", "Cameroon", "Canada","Chad", "Chile", "China", "Colombia", "Congo", 
@@ -159,8 +141,51 @@ def countriesList(year):
                      "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", 
                      "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Trinidad and Tobago", "Tunisia", "Turkey",
                      "Turkmenistan", "Uganda", "Ukraine", "UAE", "United Kingdom", "United States", "Uruguay", 
-                     "Uzbekistan", "Venezuela", "Vietnam", "Zimbabwe"]
-    return countries
+                     "Uzbekistan", "Venezuela", "Vietnam", "Zimbabwe"]}, 
+                {"year" : 2010, "countries" : ["Afghanistan", "Albania", "Algeria", "Angola", "Argentina", "Armenia", "Australia", 
+                     "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", 
+                     "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia", "Brazil", "Bulgaria", 
+                     "Cambodia", "Cameroon", "Canada","Chad", "Chile", "China", "Colombia", "Congo", 
+                     "Costa Rica", "Croatia", "Cuba", "Czech Republic", "Denmark", "Dominican Republic", 
+                     "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Finland", 
+                     "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Guatemala", 
+                     "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", 
+                     "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", 
+                     "Kazakhstan", "Kenya", "North Korea", "South Korea", "Kosovo", "Kuwait", "Kyrgyzstan", 
+                     "Laos", "Latvia", "Lebanon", "Libya", "Lithuania", "Madagascar", "Malaysia", "Malta", 
+                     "Mexico", "Moldova", "Mongolia", "Morocco", "Myanmar", "Nepal", "Netherlands", "New Zealand", 
+                     "Nicaragua", "Niger", "Nigeria", "Norway", "Pakistan", "Panama", "Paraguay", "Peru", 
+                     "Philippines", "Poland", "Portugal", "Romania", "Russia", "Rwanda", "Saudi Arabia", 
+                     "Senegal", "Serbia", "Singapore", "Slovakia", "Slovenia", "Somalia", "South Africa", 
+                     "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", 
+                     "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Trinidad and Tobago", "Tunisia", "Turkey",
+                     "Turkmenistan", "Uganda", "Ukraine", "UAE", "United Kingdom", "United States", "Uruguay", 
+                     "Uzbekistan", "Venezuela", "Vietnam", "Zimbabwe"]}, 
+                {"year" : 2020, "countries" : ["Afghanistan", "Albania", "Algeria", "Angola", "Argentina", "Armenia", "Australia", 
+                     "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", 
+                     "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia", "Brazil", "Bulgaria", 
+                     "Cambodia", "Cameroon", "Canada","Chad", "Chile", "China", "Colombia", "Congo", 
+                     "Costa Rica", "Croatia", "Cuba", "Czech Republic", "Denmark", "Dominican Republic", 
+                     "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Finland", 
+                     "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Guatemala", 
+                     "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", 
+                     "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", 
+                     "Kazakhstan", "Kenya", "North Korea", "South Korea", "Kosovo", "Kuwait", "Kyrgyzstan", 
+                     "Laos", "Latvia", "Lebanon", "Libya", "Lithuania", "Madagascar", "Malaysia", "Malta", 
+                     "Mexico", "Moldova", "Mongolia", "Morocco", "Myanmar", "Nepal", "Netherlands", "New Zealand", 
+                     "Nicaragua", "Niger", "Nigeria", "Norway", "Pakistan", "Panama", "Paraguay", "Peru", 
+                     "Philippines", "Poland", "Portugal", "Romania", "Russia", "Rwanda", "Saudi Arabia", 
+                     "Senegal", "Serbia", "Singapore", "Slovakia", "Slovenia", "Somalia", "South Africa", 
+                     "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", 
+                     "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Trinidad and Tobago", "Tunisia", "Turkey",
+                     "Turkmenistan", "Uganda", "Ukraine", "UAE", "United Kingdom", "United States", "Uruguay", 
+                     "Uzbekistan", "Venezuela", "Vietnam", "Zimbabwe"]}]
+
+def countriesList(year):
+    for arr in countriesArr:
+        if arr["year"] == year:
+            return arr["countries"]
+            break
 
 
 def allCountries(year):
@@ -191,6 +216,7 @@ def allCountries(year):
         pages.append("\n".join(countries[100:120]))
         pages.append("\n".join(countries[120:len(countries)])) #max pages = 7
     return pages
+
 
 def countryExists(country, year):
     countryList = countriesList(year)
